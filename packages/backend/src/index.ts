fe9566424d51f8b094c71d9340286fd491b56ed3
EthanSherr <esherrthan@gmail.com>
2024-11-09 14:53:15 -0500

    adding trpc neato

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,7 +1,32 @@
-import { hello } from "@word-games/common/src/hello";
+import { createExpressMiddleware } from "@trpc/server/adapters/express";
+import cors from "cors";
+import { appRouter } from "./router";
+import express from "express";
 
 const main = () => {
-  const phrase = hello("may");
-  console.log(phrase);
+  const app = express();
+
+  app.use(
+    cors({
+      origin: true, // allows all origins
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // allows all common HTTP methods
+      credentials: true, // allows cookies and credentials
+    }),
+  );
+
+  app.use(
+    "/trpc",
+    createExpressMiddleware({
+      router: appRouter,
+      createContext: () => ({}),
+    }),
+  );
+
+  const port = 4000;
+
+  app.listen(port, () => {
+    console.log("server listening to port", port);
+  });
 };
+
 main();
--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,9 +1,14 @@
 // src/App.tsx
 import React from "react";
+import { trpc } from "./connection/TrpcQueryContextProvider";
 
 const App: React.FC = () => {
+  const { data, isLoading, error } = trpc.getPyramidGameToday.useQuery();
+
   return (
     <div>
+      {isLoading && "loading..."}
+      {data && <pre>{JSON.stringify(data, null, 4)}</pre>}
       <h1>Goodbye, Vite + React + TypeScript!</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
