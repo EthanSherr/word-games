fdb9c2bd52ad0a23e7e42813269266cdfb537267
EthanSherr <esherrthan@gmail.com>
2024-11-09 12:34:44 -0500

    first commit

---
+++ packages/frontend/src/App.tsx
@@ -0,0 +1,13 @@
+// src/App.tsx
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Goodbye, Vite + React + TypeScript!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ readme.md
@@ -0,0 +1,5 @@
+# word-games
+
+This is a project for may and ethan. it will contain maybe multiple word games? but for starts it'll just contain one word game.
+
+##
