92a88662da7f8ea6a409a4cbafb8305f1a461028
EthanSherr <esherrthan@gmail.com>
2024-12-24 16:30:27 -0500

    save

--- packages/common/src/word-utils/anagramLookup.ts
+++ packages/common/src/word-utils/anagramLookup.ts
@@ -1,22 +1,22 @@
 export const makeAnagramLookup = () => {
-  const anagramMap = new Map<string, Set<string>>();
+  const anagramMap = new Map<string, Set<string>>()
 
-  const toHash = (word: string) => word.split("").sort().join("");
+  const toHash = (word: string) => word.split("").sort().join("")
   // insert adds the word itself
   const insert = (word: string) => {
-    const hash = toHash(word);
-    const anagramGroup = anagramMap.get(hash) ?? new Set();
-    anagramGroup.add(word);
-    anagramMap.set(hash, anagramGroup);
-  };
-  const getAnagrams = (word: string) => anagramMap.get(word);
+    const hash = toHash(word)
+    const anagramGroup = anagramMap.get(hash) ?? new Set()
+    anagramGroup.add(word)
+    anagramMap.set(hash, anagramGroup)
+  }
+  const getAnagrams = (word: string) => anagramMap.get(toHash(word))
 
-  const getAll = () => anagramMap;
+  const getAll = () => anagramMap
 
   return {
     insert,
     getAnagrams,
     getAll,
-  };
-};
-export type AnagramLookup = ReturnType<typeof makeAnagramLookup>;
+  }
+}
+export type AnagramLookup = ReturnType<typeof makeAnagramLookup>
